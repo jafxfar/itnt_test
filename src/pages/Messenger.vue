15075bf8a94124e74633f201d7b3c69c92df2585
jafxfar <jamshedi.j@karimov.group>
2024-05-26 18:08:31 +0500

    avatar project no fixed

--- src/pages/Messenger.vue
+++ src/pages/Messenger.vue
@@ -1,13 +1,13 @@
 <template>
     <Header showUserMinify :routeName="'Чаты'" />
     <v-container grid-list-xs>
-        <Chat />
+        <!-- <Chat /> -->
     </v-container>
     <Footer />
 </template>
 
 <script setup lang="ts">
-import Chat from '~/components/ChatPage/ChatContent.vue'
+// import Chat from '~/components/ChatPage/ChatContent.vue'
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
 
