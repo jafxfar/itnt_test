c6ca1fcaa7ca614c0d8122c8b3da4fde78af7992
jafxfar <jamshedi.j@karimov.group>
2024-06-26 12:28:44 +0500

    draggable - in addPhoto

--- src/components/projects/ProjectAddPhoto.vue
+++ src/components/projects/ProjectAddPhoto.vue
@@ -6,77 +6,52 @@
         </div>
 
         <!-- Форма загрузки фотографий -->
-        <draggable class="photo-upload" v-model="imageUrls" @end="onEnd">
-            <template #item="{ element, index }">
-                <div :key="index" class="upload-wrapper">
-                    <label :for="inputIds[index]"
-                        :class="['file-upload-label', { 'with-image': element }]"
-                        :style="{ backgroundImage: element ? 'url(' + element + ')' : 'none' }">
-                        <input :id="inputIds[index]" type="file" @change="handleFileChange($event, index)"
-                            accept="image/*" />
-                        <span class="icon" v-show="!element">
-                            <v-icon icon="mdi-plus" />
-                        </span>
-                        <div>
-                            <!-- Отображение иконки, если файл не является изображением и был добавлен -->
-                            <span class="file-icon flex justify-center"
-                                v-show="fileNames[index] && !isImageType(fileTypes[index])">
-                                <img :src="file" alt="">
-                            </span>
-                            <!-- Отображение имени файла внутри блока фотографии, если файл не является изображением -->
-                            <p class="text-center" v-show="fileNames[index] && !isImageType(fileTypes[index])">{{ truncateFileName(fileNames[index]) }}</p>
-                        </div>
-                        <button v-show="fileNames[index]" @click="deleteFile(index)" class="delete p-1">
-                            <div class="rounded-[50%] bg-[#FFEBEE] p-[2px]">
-                                <v-icon icon="mdi-close" />
-                            </div>
-                        </button>
-                    </label>
-                </div>
-            </template>
-        </draggable>
-        <div class="text-center">
-            <v-snackbar color="white" v-model="snackbar" multi-line>
-                {{ text }}
-                <template v-slot:actions v-if="showContent">
-                    <v-btn color="#29B6F6" variant="text" @click="snackbar = false">Отменить</v-btn>
-                </template>
-            </v-snackbar>
+        <div class="photo-upload flex w-full flex-wrap">
+            <div v-for="(inputId, index) in inputIds" :key="index" class="upload-wrapper">
+                <label :for="inputId"
+                    :class="['file-upload-label', { 'with-image': imageUrls[index], 'active': index === activeIndex }]"
+                    :style="{ backgroundImage: 'url(' + imageUrls[index] + ')' }">
+                    <input :id="inputId" type="file" @change="handleFileChange($event, index)" accept="image/*"
+                        :disabled="index !== activeIndex" />
+                    <span class="icon" v-show="!imageUrls[index]">
+                        <v-icon icon="mdi-plus" />
+                    </span>
+                    <div class="">
+                        <!-- Отображение иконки, если файл не является изображением и был добавлен -->
+                        <span class="file-icon flex justify-center" v-show="fileNames[index] && !isImageType(fileTypes[index])">
+                       <img :src="file" alt="">
+                    </span>
+                    <!-- Отображение имени файла внутри блока фотографии, если файл не является изображением -->
+                    <p class="text-center" v-show="fileNames[index] && !isImageType(fileTypes[index])">{{ truncateFileName(fileNames[index])
+                        }}</p>
+                    </div>
+                    
+                </label>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import file from "~/assets/icons/media/ppt-blue.svg";
-import draggable from 'vuedraggable';
-
+import { ref } from 'vue';
+import file from "~/assets/icons/media/ppt-blue.svg"
 const inputIds = Array.from({ length: 8 }, (_, i) => `file-upload-${i}`);
-const imageUrls = ref(Array(8).fill('')); // Инициализируем пустыми строками вместо null
-const fileTypes = ref(Array(8).fill(''));
+const imageUrls = ref(Array(8).fill(null));
+const activeIndex = ref(0);
+const fileTypes = ref(Array(8).fill(null));
 const fileTypeText = ref('');
-const fileNames = ref(Array(8).fill(''));
-const snackbar = ref(false);
-const showContent = ref(false);
-const text = ref('I am a multi-line snackbar.\nI can have more than one line. This is another line that is quite long.');
-
-watch(snackbar, (newValue) => {
-    if (newValue) {
-        showContent.value = false;
-        setTimeout(() => {
-            showContent.value = true;
-        }, 3000);
-    }
-});
+const fileNames = ref(Array(8).fill(null));
 
 const handleFileChange = (event: Event, index: number) => {
     const file = (event.target as HTMLInputElement).files[0];
     if (file) {
         if (!file.type.startsWith('image/')) {
+            console.log('Имя файла: ', file.name);
             fileNames.value[index] = file.name;
         } else {
-            fileNames.value[index] = '';
+            fileNames.value[index] = null;
         }
+        console.log('Тип файла: ', file.type);
         fileTypes.value[index] = file.type;
         fileTypeText.value = file.type;
         fileNames.value[index] = file.name;
@@ -85,12 +60,22 @@ const handleFileChange = (event: Event, index: number) => {
         reader.onload = () => {
             if (reader.result) {
                 imageUrls.value[index] = reader.result.toString();
+                setNextActive(index);
             }
         };
         reader.readAsDataURL(file);
     }
 };
 
+const setNextActive = (index: number) => {
+    if (index < inputIds.length - 1) {
+        activeIndex.value = index + 1;
+    } else {
+        activeIndex.value = 0;
+    }
+};
+
+// Функция для укорачивания имени файла
 const truncateFileName = (fileName: string): string => {
     if (fileName && fileName.length > 7) {
         return fileName.substring(0, 7) + '...';
@@ -98,27 +83,18 @@ const truncateFileName = (fileName: string): string => {
     return fileName;
 };
 
+// Функция для подсчета загруженных фотографий
 const countUploadedPhotos = () => {
-    return imageUrls.value.filter(url => url !== '').length;
+    return imageUrls.value.filter(url => url !== null).length;
 };
 
+// Функция для проверки типа файла на изображение
 const isImageType = (type: string | null): boolean => {
     return type ? type.startsWith('image/') : false;
 };
-
-const deleteFile = (index: number) => {
-    imageUrls.value[index] = '';
-    fileNames.value[index] = '';
-    fileTypes.value[index] = '';
-    snackbar.value = true;
-};
-
-const onEnd = (event: any) => {
-    console.log('Sorted images:', imageUrls.value);
-};
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .photo-upload {
     display: flex;
     flex-wrap: wrap;
@@ -127,14 +103,13 @@ const onEnd = (event: any) => {
 }
 
 .upload-wrapper {
-    flex-basis: calc(25% - 12px); /* 2 колонки */
+    flex-basis: calc(25% - 12px);
 }
 
 .file-upload-label {
     display: flex;
-    position: relative;
-    width: 100%;
-    height: 220px;
+    min-width: 100%;
+    min-height: 220px;
     background-color: white;
     cursor: pointer;
     padding: 10px;
@@ -144,28 +119,52 @@ const onEnd = (event: any) => {
     border-radius: 12px;
     margin-bottom: 10px;
     background-size: cover;
+    cursor: default;
     background-position: center;
     box-sizing: border-box;
     box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.05);
-    transition: transform 0.3s ease; /* Добавление плавности */
 }
 
-.upload-wrapper.dragging {
-    transition: transform 0.3s ease; /* Добавление плавности */
+@media (max-width: 576px) {
+    .file-upload-label {
+        min-height: 110px;
+    }
 }
 
-.delete {
-    position: absolute;
-    right: -8px;
-    bottom: -8px;
-    background-color: white;
-    border-radius: 50%;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.05);
+@media (max-width: 440px) {
+    .file-upload-label {
+        min-height: 110px;
+    }
 }
+
+@media (max-width: 380px) {
+    .file-upload-label {
+        min-height: 100px;
+    }
+}
+
 
 .file-upload-label.with-image {
-    height: 220px;
-    width: 100%;
+    min-height: 220px;
+    min-width: 100%;
+}
+
+@media (max-width: 576px) {
+    .file-upload-label.with-image {
+        min-height: 110px;
+    }
+}
+
+@media (max-width: 440px) {
+    .file-upload-label.with-image {
+        min-height: 110px;
+    }
+}
+
+@media (max-width: 380px) {
+    .file-upload-label.with-image {
+        min-height: 100px;
+    }
 }
 
 .file-upload-label.active {
@@ -181,15 +180,5 @@ input[type="file"] {
     justify-content: center;
     align-items: center;
     font-size: 24px;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.5s;
-}
-
-.fade-enter,
-.fade-leave-to {
-    opacity: 0;
 }
 </style>
