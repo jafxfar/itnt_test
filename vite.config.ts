80e339146f13569b577896cf34f9793f5009a92f
jafxfar <jamshedi.j@karimov.group>
2024-06-26 12:46:47 +0500

    socket fix+-

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     build: {
         minify: true,
         rollupOptions: {
-            external: ['socket.io-client', 'vuedraggable']
+            external: ['', 'vuedraggable']
           }
     },
 
